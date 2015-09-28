36cd98f3b2ed8ab9ae63b65edc7deda313b3c7ed
Andrew Faircloth <andrewkfaircloth@gmail.com>
2015-09-28 15:59:13 -0400

    completed assignment

--- main.js
+++ main.js
@@ -2,23 +2,22 @@
     Part 1: Variables
 */
 var givenName;
-// Q: What does `givenName` equal right now?
-// A:
 
+// Q: What does `givenName` equal right now?
+// A: undefined
 
 givenName = "Tim";
 // Q: What is `givenName` set to right now?
-// A:
+// A: Tim
 
 
 givenName = "JD";
 // Q: What is `givenName` set to right now?
-// A:
-
+// A: JD
 
 var greeting = "Hello, how are you?";
 // Q: What is `greeting` set to?
-// A:
+// A: "Hello, how are you?"
 
 
 /*=========================================================
@@ -31,11 +30,11 @@ var low  = 10;
 
 var math = high - low;
 // Q: What is `math` set to?
-// A:
+// A: 40
 
 math = high - "5";
 // Q: What is `math` set to?
-// A:
+// A: 45
 
 
 /* =========================================================
@@ -49,16 +48,25 @@ var born = 1983;
 var today = 2015;
 
 // Answers Below:
-
+var age = today - born;
+var timsAge = "Tim is " + age + " years old";
+timsAge;
+"Tim is 32 years old"
 
 
 // Store some information following in variables.
 var yourName;
 var instructorName;
 
+
 // Update the variables above so the expression reads correctly.
 // Answers Below:
-
+yourName = "Andrew"
+instructorName = "Tim"
 
 // Final Statement
 var statement = yourName + " is taking a class at The Iron Yard, my instructor's name is " + instructorName;
+statement;
+Andrew is taking a class at The Iron Yard, my instructor's name is Tim
+
+
